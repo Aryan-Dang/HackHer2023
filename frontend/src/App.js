e2ded15f9a934490872994b24e5b6fba39588ea5
vivjamba <85517762+vivjamba@users.noreply.github.com>
2023-02-26 11:16:30 -0500

    image and attempt to center

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,6 @@ import { useState } from "react"
 import ProgressBar from "./ProgressBar.component.js";
 import logo from "./img/SipBudLogo.png"
 
-// const testData = [
-//   { bgcolor: "#6a1b9a", completed: 60 },
-//   { bgcolor: "#00695c", completed: 30 },
-//   { bgcolor: "#ef6c00", completed: 53 },
-// ];
-
 function App() {
   const [posts, setPosts] = useState([])
 
@@ -30,11 +24,19 @@ function App() {
   }
 
   return (
-    <div className="container mx-auto p-4 m-4 border-solid border-2 border-gray-600 bg-gray-200">
+    <div className="container mx-auto p-4 m-4 border-solid border-10 border-gray-600 bg-gray-200" >
       <img src={logo} width="300px"></img>
-      {/* <h1 className="text-4xl text-gray-800 py-2 text-center">
-        SipBud
-      </h1> */}
+      <table className="table-auto py-4 border-gray-500">
+
+      Daily Goal: {posts.intakeGoal}ml
+      <div className="App">
+      <ProgressBar key="bar" bgcolor={"#0081cc"} completed={posts.progress} />
+      You are {posts.progress}% of the way to your daily goal
+      {/* {testData.map((item, idx) => (
+        <ProgressBar key={idx} bgcolor={item.bgcolor} completed={item.completed} />
+      ))} */}
+    </div>
+      </table>
       <ul className="flex flex-wrap py-4">
           <button
             type="button"
@@ -44,23 +46,12 @@ function App() {
             Refresh
           </button>
       </ul>
-      <table className="table-auto py-4 border-gray-500">
-
-      <ul>
-        <li>Daily Goal: {posts.intakeGoal}</li>
-        {/* <li>Progress: {posts.progress}</li> */}
-      </ul>
-      <div className="App">
-      <ProgressBar key="bar" bgcolor={"#0081cc"} completed={posts.progress} />
-      You are {posts.progress}% of the way to the daily goal
-      {/* {testData.map((item, idx) => (
-        <ProgressBar key={idx} bgcolor={item.bgcolor} completed={item.completed} />
-      ))} */}
-    </div>
-      </table>
     </div>
     
   )
 }
 
 export default App
+
+
+
